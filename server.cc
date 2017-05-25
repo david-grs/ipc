e42da4a132b7fbec475aecd8f70c41f710709aaa
David G <david.a.grs@gmail.com>
2017-05-26 00:16:36 +0200

    server: active loop on microsecond

--- server.cc
+++ server.cc
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     server.start();
     while (1)
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
         server.update();
     }
     return 0;
