f5a6015250c4d2225091932eb73e5621c2dacd43
David G <david.a.grs@gmail.com>
2017-05-26 00:03:30 +0200

    active wait

--- server.cc
+++ server.cc
@@ -1,8 +1,13 @@
 #include "shm_server.h"
 
+#include <thread>
+#include <chrono>
+
 int main(int argc, char *argv[])
 {
     shm_server server("foob4r");
     server.start();
+    while (1)
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     return 0;
 };
