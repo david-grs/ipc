aec6fa3603f9bf3f7c3c1579b2bf8e4ece5495e3
David G <david.a.grs@gmail.com>
2017-05-26 00:12:46 +0200

    server: update(2)

--- server.cc
+++ server.cc
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
     shm_server server("foob4r");
     server.start();
     while (1)
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        server.update();
+    }
     return 0;
 };
