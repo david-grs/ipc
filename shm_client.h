441c9219970c708a3da7d275b6bc00d8aba46016
David G <david.a.grs@gmail.com>
2017-05-26 00:15:15 +0200

    client: check if all values from server are consecutive

--- shm_client.h
+++ shm_client.h
@@ -24,9 +24,17 @@ struct shm_client
 
         shm_vector* v = segment.find<shm_vector>("shm_vector").first;
 
-        std::sort(v->rbegin(), v->rend());
-        for (const auto& x : *v)
-            std::cout << "child = " << x << std::endl;
+        int last = (*v)[0];
+        for (int i = 1; i < 10; ++i)
+        {
+            const int elem = (*v)[i];
+
+            if (elem != last + 1)
+                throw 42;
+
+            std::cout << "elem = " << elem << std::endl;
+            last = elem;
+        }
 
         //segment.destroy<shm_vector>("shm_vector");
     }
