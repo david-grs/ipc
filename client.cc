cb68afd4751c419fe753e91e8ec9fb68dc1f4f31
David G <david.a.grs@gmail.com>
2017-05-27 00:23:20 +0200

    client: active loop

--- client.cc
+++ client.cc
@@ -1,8 +1,25 @@
 #include "shm_client.h"
 
+#include <chrono>
+
 int main(int argc, char *argv[])
 {
     shm_client client("foob4r");
     client.start();
+
+    auto start = std::chrono::steady_clock::now();
+    while (1)
+    {
+        for (int i = 0; i < 100; ++i)
+            client.read();
+
+        auto now = std::chrono::steady_clock::now();
+        if (now - start > std::chrono::seconds(1))
+        {
+            std::cout << double(client.count()) / std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count() << "ops/ms" << std::endl;
+            start = now;
+            client.reset();
+        }
+    }
     return 0;
 };
