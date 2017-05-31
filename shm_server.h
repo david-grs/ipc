9e89063a8257b3413d1b4ec9d00cd909ae81065c
David G <david.a.grs@gmail.com>
2017-05-31 23:51:29 +0200

    server: adding construct<T>

--- shm_server.h
+++ shm_server.h
@@ -4,10 +4,12 @@
 
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
+#include <boost/any.hpp>
 
 #include <memory>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 namespace shm { namespace detail {
 
@@ -32,26 +34,45 @@ struct server
 
         _segment = std::make_unique<ipc::managed_shared_memory>(ipc::create_only, _name.c_str(), 65536);
         _alloc = std::make_unique<const void_allocator>(_segment->get_segment_manager());
+    }
 
+    template <typename Object>
+    void construct(const std::string& name)
+    {
+        Object* obj = _segment->construct<Object>(name.c_str())(*_alloc);
+        const bool inserted = _objects.emplace(name, obj);
+
+        if (!inserted)
+            throw std::runtime_error("shm: object " + name + " already inserted");
+    }
+
+    template <typename T, typename Callable>
+    void modify(const std::string& name)
+    {
+    }
+
+#if 0
         _data = _segment->construct<shared_data>("blarp")(*_alloc);
         for (int i = 0; i < 10; ++i)
             _data->_shm_vector.push_back(i);
 
         _data->_shm_map.emplace(shared_data::shm_string("foo", *_alloc), data{10.0, 3});
         _data->_shm_map.emplace(shared_data::shm_string("bar", *_alloc), data{4.0, 11});
-    }
+#endif
 
     int count() const { return _updates; }
     void reset() { _updates = 0; }
 
     void update()
     {
+#if 0
         ipc::scoped_lock<ipc::interprocess_upgradable_mutex> lock{_data->_mutex};
 
         for (int i = 0; i < 10; ++i)
             _data->_shm_vector[i] = _data->_shm_vector[i] + 1;
 
         ++_updates;
+#endif
     }
 
 
@@ -59,7 +80,7 @@ private:
     const std::string _name;
     std::unique_ptr<ipc::managed_shared_memory> _segment;
     std::unique_ptr<const void_allocator> _alloc;
-    shared_data* _data;
+    std::unordered_map<std::string, boost::any> _objects;
     int _updates = {};
 };
 
