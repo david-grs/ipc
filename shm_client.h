cf8c1da1fb4329ddd6b4f19e2e1a2916f924abe5
David G <david.a.grs@gmail.com>
2017-06-05 23:52:33 +0200

    client:removing dead code

--- shm_client.h
+++ shm_client.h
@@ -28,37 +28,6 @@ struct client
         return _segment->find<data<Object>>(name.c_str()).first;
     }
 
-#if 0
-    void read()
-    {
-        ipc::sharable_lock<ipc::interprocess_upgradable_mutex> lock{_data->_mutex};
-
-        int last = _data->_shm_vector[0];
-        for (int i = 1; i < 10; ++i)
-        {
-            const int elem = _data->_shm_vector[i];
-
-            if (elem != last + 1)
-                throw 42;
-
-            last = elem;
-        }
-
-        if (_data->_shm_map.size() != 2)
-            throw 42;
-
-        //segment.destroy<shm_vector>("shm_vector");
-    }
-
-    void write()
-    {
-        ipc::scoped_lock<ipc::interprocess_upgradable_mutex> lock{_data->_mutex};
-
-        for (int i = 0; i < 10; ++i)
-            _data->_shm_vector[i] = _data->_shm_vector[i] + 1;
-    }
-#endif
-
 private:
     const std::string _name;
     std::unique_ptr<ipc::managed_shared_memory> _segment;
