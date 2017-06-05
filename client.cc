da9ca70436cfd38a8554339329666de997cf5b6a
David G <david.a.grs@gmail.com>
2017-06-05 23:49:40 +0200

    client: moving read/write out

--- client.cc
+++ client.cc
@@ -2,10 +2,43 @@
 
 #include <chrono>
 
+
+void read(shm::data<shm::shared_data>& client)
+{
+    client.read([](const shm::shared_data& data)
+    {
+        int last = data._shm_vector[0];
+        for (int i = 1; i < 10; ++i)
+        {
+            const int elem = data._shm_vector[i];
+
+            if (elem != last + 1)
+                throw 42;
+
+            last = elem;
+        }
+
+        if (data._shm_map.size() != 2)
+            throw 42;
+    });
+    //segment.destroy<shm_vector>("shm_vector");
+}
+
+void write(shm::data<shm::shared_data>& client)
+{
+    client.modify([](shm::shared_data& data)
+    {
+        for (int i = 0; i < 10; ++i)
+            data._shm_vector[i] = data._shm_vector[i] + 1;
+    });
+}
+
 int main(int argc, char *argv[])
 {
     shm::client client("foob4r");
     client.start();
+
+    auto data = client.find<shm::shared_data>("shared_data");
 
     auto start = std::chrono::steady_clock::now();
     int64_t ops = 0;
@@ -13,9 +46,9 @@ int main(int argc, char *argv[])
     while (1)
     {
         for (int i = 0; i < 100; ++i)
-            client.read();
+            read(data);
         for (int i = 0; i < 100; ++i)
-            client.write();
+            write(data);
 
         auto now = std::chrono::steady_clock::now();
         if (now - start > std::chrono::seconds(1))
--- shm_client.h
+++ shm_client.h
@@ -20,7 +20,6 @@ struct client
     void start()
     {
         _segment = std::make_unique<ipc::managed_shared_memory>(ipc::open_only, _name.c_str());
-        _data = _segment->find<shared_data>("blarp").first;
     }
 
     template <typename Object>
@@ -30,6 +29,7 @@ struct client
         return data<Object>(obj);
     }
 
+#if 0
     void read()
     {
         ipc::sharable_lock<ipc::interprocess_upgradable_mutex> lock{_data->_mutex};
@@ -58,12 +58,11 @@ struct client
         for (int i = 0; i < 10; ++i)
             _data->_shm_vector[i] = _data->_shm_vector[i] + 1;
     }
-
+#endif
 
 private:
     const std::string _name;
     std::unique_ptr<ipc::managed_shared_memory> _segment;
-    shared_data* _data;
 };
 
 }
