3b1feae405637b33bf50af1c9caf4639c11a171c
David G <david.a.grs@gmail.com>
2017-06-10 13:19:49 +0200

    signal handling for cleaner exit

--- server.cc
+++ server.cc
@@ -3,6 +3,14 @@
 #include <thread>
 #include <chrono>
 
+extern "C"
+{
+#include <signal.h>
+}
+
+// boo
+volatile bool run = true;
+
 int main(int argc, char *argv[])
 {
     shm::server server("foob4r");
@@ -23,7 +31,10 @@ int main(int argc, char *argv[])
     auto start = std::chrono::steady_clock::now();
     int64_t ops = 0;
 
-    while (1)
+    ::signal(15, [](int) { run = false; });
+    ::signal(2, [](int)  { run = false; });
+
+    while (run)
     {
         for (int i = 0; i < 100; ++i)
         {
