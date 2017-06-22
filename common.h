4c2d82ae13583869360ba9bdeb65691e593df41d
David G <david.a.grs@gmail.com>
2017-06-22 23:39:42 +0200

    adding examples about types

--- common.h
+++ common.h
@@ -12,10 +12,16 @@ struct shared_data
 {
     explicit shared_data(const shm::void_allocator& sm) :
       _shm_map(std::less<shm::string>(), sm),
-      _shm_vector(sm)
+      _shm_set(std::less<shm::string>(), sm),
+      _shm_list(sm),
+      _shm_vector(sm),
+      _shm_svector(sm)
     {}
 
     shm::map<shm::string, mmdata> _shm_map;
+    shm::set<shm::string> _shm_set;
+    shm::list<shm::string> _shm_list;
     shm::vector<int> _shm_vector;
+    shm::stable_vector<int> _shm_svector;
 };
 
